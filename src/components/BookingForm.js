4c23ff2796819acd209f5e0ae60aa04fc557d2b0
Fadi Buni <fadibuni1@gmail.com>
2024-09-11 15:33:35 -0700

    Added the reservation page

---
+++ src/components/BookingForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Button from "./Button";
+function BookingForm() {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState(1);
+  const [occasion, setOccasion] = useState("Birthday");
+
+  const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00"];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ date, time, guests, occasion });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
+    >
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      >
+        {availableTimes.map((availableTime) => (
+          <option key={availableTime} value={availableTime}>
+            {availableTime}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        id="guests"
+        value={guests}
+        onChange={(e) => setGuests(e.target.value)}
+        min="1"
+        max="10"
+      />
+
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
+      >
+        <option value="Birthday">Birthday</option>
+        <option value="Anniversary">Anniversary</option>
+      </select>
+
+      <Button text={"Reserve"} />
+    </form>
+  );
+}
+
+export default BookingForm;
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,9 +1,13 @@
-const BookingPage = () => {
+import React from 'react';
+import BookingForm from './BookingForm';
+
+function BookingPage() {
   return (
     <div>
-      <h1>Booking Page</h1>
+      <h1>Reserve a Table</h1>
+      <BookingForm />
     </div>
   );
-};
+}
 
 export default BookingPage;
